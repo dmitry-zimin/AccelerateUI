afb9639d2bd0cbad1cae432a22986d445e7bce13
dmitry-zimin <dmitry.super.user@gmail.com>
2013-12-08 03:32:42 +0200

    js parser matcher fixes

--- lib/parsers/css_parser.rb
+++ lib/parsers/css_parser.rb
@@ -1,3 +1,16 @@
 class CssParser
+  def initialize
+    @id_array = []
+    @class_array = []
+  end
+
+  def id_parser(file_content)
+    @id_array = file_content.scan(/\#[^'",.\s]*/)
+  end
+
+  def class_parser(file_content)
+    @class_array = file_content.scan(/\.[^'".,\s]*/)
+
+  end
 
 end
--- lib/parsers/js_parser.rb
+++ lib/parsers/js_parser.rb
@@ -5,10 +5,12 @@ class JsParser
   end
 
   def id_parser(file_content)
-    @id_array = file_content.scan(/#[^'".\s]*/)
+    @id_array = file_content.scan(/\#[^'".,\s]*/)
   end
 
   def class_parser(file_content)
+    #TODO: make this shit working, for now this matcher not cool enough
+    @class_array = file_content.scan(/\.[^'",.\s]/*)
 
   end
 end
